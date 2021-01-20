343119e9c8b593cc2a707dff69c15a505df27298
RayAratrika <54719944+RayAratrika@users.noreply.github.com>
2021-01-20 11:43:57 +0530

    youtube-search-python package

--- last_fm.py
+++ last_fm.py
@@ -1,6 +1,9 @@
 import keys;
 import requests as req;
-from youtube_search import YoutubeSearch as yt;
+import grequests as greq;
+import time;
+
+from youtubesearchpython import VideosSearch;
 
 api_key = keys.lfm_api_key();
 user_agent = keys.lfm_user_agent();
@@ -8,42 +11,38 @@ user_agent = keys.lfm_user_agent();
 
 #chart top tracks
 def chart():
-	i = 1;
+	i = 0;
 	url = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" + api_key + "&format=json";
 	res = req.get(url);
 	tracks = [];
 	for r in res.json()['tracks']['track']:
-		if(i == 11): break;
-		track_url = yt((str(r['name']) + " - " + str(r['artist']['name']) + " lyrics"), max_results=1).to_dict();
-		for t in track_url:
-			yt_url = "https://www.youtube.com/watch?v=" + str(t['id']);
-		text = str(r['name']) + " - " + str(r['artist']['name']) + "\n" + yt_url + "\n";
-		tracks.append(text);
+		searches = VideosSearch((str(r['name']) + " - " + str(r['artist']['name']) + " lyrics"), limit = 1);
+		for v in searches.result()['result']:
+			tracks.append(str(r['name']) + " - " + str(r['artist']['name']) + "\n" + v['link'] + "\n");
 		i += 1;
+		if(i+1 == 11): break;
 	return tracks;
 
 
 #songs by artist
 def artist(artist):
-	i = 1;
+	i = 0;
 	url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artist +"&api_key=" + api_key + "&format=json"
 	res = req.get(url);
 	artist_track = [];
 	for r in res.json()['toptracks']['track']:
-		if(i == 11): break;
-		track_url = yt((str(r['name']) + " - " + artist + " lyrics"), max_results=1).to_dict();
-		for t in track_url:
-			yt_url = "https://www.youtube.com/watch?v=" + str(t['id']);
-		text = str(r['name']) + " - " + yt_url + "\n";
-		artist_track.append(text);
-		i += 1; print(i);
+		searches = VideosSearch((str(r['name']) + " - " + artist + " lyrics"), limit = 1);
+		for v in searches.result()['result']:
+			artist_track.append(str(r['name']) + " - " + v['link'] + "\n");
+		i += 1;
+		if(i+1 == 11): break;
 	return artist_track;
 
 
 #get genre playlist from last.fm
 def genre(genre):
 	links = [];
-	yt_url = yt(genre+" playlist", max_results=10).to_dict();
-	for r in yt_url: 
-		links.append("https://www.youtube.com/watch?v=" + str(r['id']) + " - " + str(r['title']) +"\n");
+	searches = VideosSearch(genre+" playlist", limit=10);
+	for v in searches.result()['result']:
+		links.append(str(v['link']) + " - " + str(v['title']) + "\n");
 	return links;
