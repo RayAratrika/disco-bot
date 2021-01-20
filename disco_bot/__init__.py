59302c6882efc72cf980da158703d29bfec8c340
Anirudh Gupta <anirudhgupta109@aosip.dev>
2021-01-20 11:47:00 +0530

    discord-bot: treewide: Spaces2tabs
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- disco_bot/__init__.py
+++ disco_bot/__init__.py
@@ -1,8 +1,8 @@
 import sys
 # if version < 3.8, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 8:
-    LOGGER.error("You MUST have a python version of at least 3.8! Multiple features depend on this. Bot quitting.")
-    quit(1)
+	LOGGER.error("You MUST have a python version of at least 3.8! Multiple features depend on this. Bot quitting.")
+	quit(1)
 
 from disco_bot.config import Development as Config
 api_key = Config.API_KEY
--- disco_bot/sample_config.py
+++ disco_bot/sample_config.py
@@ -1,27 +1,27 @@
 # Ensure default configs exist to prevent unnecessary bail outs and API calls
 if not __name__.endswith("sample_config"):
-    import sys
-    print("The README is there to be read. Extend this sample_config to a seperate config.py file, don't just rename and change "
-          "values here.\nBot quitting.", file=sys.stderr)
-    quit(1)
+	import sys
+	print("The README is there to be read. Extend this sample_config to a seperate config.py file, don't just rename and change "
+		"values here.\nBot quitting.", file=sys.stderr)
+	quit(1)
 
 
 # Create a new config.py file in same dir and import, then extend this class.
 class Config(object):
-    LOGGER = True
+	LOGGER = True
 
-    # REQUIRED
-    BOT_TOKEN = "YOUR TOKEN HERE"
-    API_KEY = "YOUR KEY HERE"
-    USER_AGENT = "YOUR NAME HERE"
+	# REQUIRED
+	BOT_TOKEN = "YOUR TOKEN HERE"
+	API_KEY = "YOUR KEY HERE"
+	USER_AGENT = "YOUR NAME HERE"
 
-    # OPTIONAL
-    BOT_COLOR = 0xFFA000
+	# OPTIONAL
+	BOT_COLOR = 0xFFA000
 
 
 class Production(Config):
-    LOGGER = False
+	LOGGER = False
 
 
 class Development(Config):
-    LOGGER = True
+	LOGGER = True
