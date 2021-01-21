8c2283d7d3f2a4c684ae96bf190459e4031b2f41
RayAratrika <54719944+RayAratrika@users.noreply.github.com>
2021-01-21 12:34:37 +0530

    Taking care of errors

--- last_fm.py
+++ last_fm.py
@@ -2,12 +2,13 @@ import keys;
 import requests as req;
 import grequests as greq;
 import time;
+import threading;
+import sys;
 
 from youtubesearchpython import VideosSearch;
 
 api_key = keys.lfm_api_key();
 user_agent = keys.lfm_user_agent();
-
 
 #chart top tracks
 def chart():
@@ -15,34 +16,45 @@ def chart():
 	url = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" + api_key + "&format=json";
 	res = req.get(url);
 	tracks = [];
-	for r in res.json()['tracks']['track']:
-		searches = VideosSearch((str(r['name']) + " - " + str(r['artist']['name']) + " lyrics"), limit = 1);
-		for v in searches.result()['result']:
-			tracks.append(str(r['name']) + " - " + str(r['artist']['name']) + "\n" + v['link'] + "\n");
-		i += 1;
-		if(i+1 == 11): break;
+	try:
+		for r in res.json()['tracks']['track']:
+			# takes and avg of 1.2s for each request w/o threading.
+			searches = VideosSearch((str(r['name']) + " - " + str(r['artist']['name']) + " lyrics"), limit = 1);
+			for v in searches.result()['result']:
+				tracks.append(str(r['name']) + " - " + str(r['artist']['name']) + "\n" + v['link'] + "\n");
+			i += 1;
+			if(i+1 == 11): break;
+	except Exception as e:
+		print(e.__class__);
+	
 	return tracks;
 
 
 #songs by artist
 def artist(artist):
 	i = 0;
-	url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artist +"&api_key=" + api_key + "&format=json"
-	res = req.get(url);
-	artist_track = [];
-	for r in res.json()['toptracks']['track']:
-		searches = VideosSearch((str(r['name']) + " - " + artist + " lyrics"), limit = 1);
-		for v in searches.result()['result']:
-			artist_track.append(str(r['name']) + " - " + v['link'] + "\n");
-		i += 1;
-		if(i+1 == 11): break;
+	try:
+		url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artist +"&api_key=" + api_key + "&format=json"
+		res = req.get(url);
+		artist_track = [];
+		for r in res.json()['toptracks']['track']:
+			searches = VideosSearch((str(r['name']) + " - " + artist + " lyrics"), limit = 1);
+			for v in searches.result()['result']:
+				artist_track.append(str(r['name']) + " - " + v['link'] + "\n");
+			i += 1;
+			if(i+1 == 11): break;
+	except KeyError: print("Artist not found");
+
 	return artist_track;
 
 
 #get genre playlist from last.fm
 def genre(genre):
 	links = [];
-	searches = VideosSearch(genre+" playlist", limit=10);
-	for v in searches.result()['result']:
-		links.append(str(v['link']) + " - " + str(v['title']) + "\n");
+	try:
+		searches = VideosSearch(genre+" playlist", limit=10);
+		for v in searches.result()['result']:
+			links.append(str(v['link']) + " - " + str(v['title']) + "\n");
+	except Exception as e: print(e.__class__);
+
 	return links;
