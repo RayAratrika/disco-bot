844bf052d3311cda958b32c9209643bba3fe8705
RayAratrika <54719944+RayAratrika@users.noreply.github.com>
2021-01-25 00:27:41 +0530

    keeping only thread functions

--- last_fm.py
+++ last_fm.py
@@ -8,13 +8,11 @@ from youtubesearchpython import VideosSearch;
 api_key = keys.lfm_api_key();
 user_agent = keys.lfm_user_agent();
 
-
-tracks = [];
+tracks = list(range(0,10));
 threads  = [];
 
 #threading function
 def thread_reqs(name, artist_name, i, func):
-	tracks.clear();
 	searches = VideosSearch((name + " - " + artist_name + " lyrics"), limit = 1);	
 	if func=="chart":
 		for v in searches.result()['result']:
@@ -62,41 +60,6 @@ def artist_t(artist):
 
 	return tracks;
 
-#chart top tracks
-def chart():
-	tracks.clear();
-	i = 0;
-	url = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" + api_key + "&format=json";
-	res = req.get(url);
-	try:
-		for r in res.json()['tracks']['track']:
-			# takes and avg of 1.2s for each request w/o threading.
-			searches = VideosSearch((str(r['name']) + " - " + str(r['artist']['name']) + " lyrics"), limit = 1);
-			for v in searches.result()['result']:
-				tracks.append(str(r['name']) + " - " + str(r['artist']['name']) + "\n" + v['link'] + "\n");
-			i += 1;
-			if i == 10: break;
-	except Exception as e:
-		print(e.__class__);
-	
-	return tracks;
-
-#songs by artist
-def artist(artist):
-	tracks.clear();
-	i = 0;
-	try:
-		url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artist +"&api_key=" + api_key + "&format=json"
-		res = req.get(url);
-		for r in res.json()['toptracks']['track']:
-			searches = VideosSearch((str(r['name']) + " - " + artist + " lyrics"), limit = 1);
-			for v in searches.result()['result']:
-				artist_track.append(str(r['name']) + " - " + v['link'] + "\n");
-			i += 1;
-			if i == 10: break;
-	except KeyError: print("Artist not found");
-
-	return tracks;
 
 #get genre playlist from last.fm
 def genre(genre):
@@ -107,4 +70,4 @@ def genre(genre):
 			links.append(str(v['link']) + " - " + str(v['title']) + "\n");
 	except Exception as e: print(e.__class__);
 
-	return tracks;
+	return links;
